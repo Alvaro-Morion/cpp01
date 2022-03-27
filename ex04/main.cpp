71220f00b725101479d923405702b33d0eb147af
Alvaro Morion de eusebio <amorion-@c1r2s4.42madrid.com>
2022-03-27 14:17:51 +0200

    ex04 newline at the end

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,17 +6,60 @@
 /*   By: amorion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 13:52:40 by amorion-          #+#    #+#             */
-/*   Updated: 2022/03/26 13:58:46 by amorion-         ###   ########.fr       */
+/*   Updated: 2022/03/27 14:16:56 by amorion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char **argv)
 {
 	std::ifstream	ifs;
-	std::ofstream	ofs
+	std::ofstream	ofs;
+	std::string		file;
+	std::string		s1;
+	std::string		s2;
+	std::string		text;
+	std::string		line;
+	std::size_t		pos = 0;;
+
 	if(argc != 4)
+	{
 		std::cerr << "Invalid number of arguments\n";
-	
+		return(1);
+	}
+	if(!argv[2] || !argv[3])
+	{
+		std::cerr << "String must not be empty\n";
+		return(1);
+	}
+	file = argv[1];
+	s1 = argv[2];
+	s2 = argv[3];
+	ifs.open(file);
+	if(!ifs.is_open())
+	{	
+		std::cerr << "Unable to open " << file << std::endl;
+		return(1);
+	}
+	ofs.open(file + ".replace");
+	if(!ofs.is_open())
+	{
+		std::cerr <<  "Unable to open " << file + ".replace" << std::endl;
+		ifs.close();
+		return(1);
+	}
+	while(getline(ifs, line))
+			text += line;
+	while((pos = text.find(s1, pos)) != text.npos)
+	{
+		text.erase(pos, s1.size());
+		text.insert(pos, s2);
+		pos += s2.size();
+	}
+	ifs.close();
+	ofs << text;
+	ofs.close();
+	return(0);
 }
